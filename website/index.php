238a4fac86522ce08a3c1eced3fb3df05becd98d
SadanChandra99 <sadansunny99@gmail.com>
2022-10-12 18:03:51 +0530

    initial commit

--- website/index.php
+++ website/index.php
@@ -5,9 +5,13 @@
 	<body>
 	    <h1>Welcome to ABCBank</h1>
 	    <h2>Candidate Information Form</h2>
+		
+		<h2>This is version 2.1</h2>
+
+		
 	    <ul>
 		<?php
-  		    $json = file_get_contents('http://product-service');
+  		    $json = file_get_contents('http://10.108.211.47:80');
   		    $obj = json_decode($json);
 		    $products = $obj->products;
 		    foreach ($products as $product) {
